a49045747fd74d325bffbc1d0183a630ceeb8780
Jitgitbit <rowowolf@gmail.com>
2020-03-11 21:03:04 +0100

    EUREKA ! HISTORY !

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 
-export default function Contact() {
+export default function Contact(props) {
+  console.log(props);
+  setTimeout(() => {
+    props.history.push('/about')
+  }, 2000);
   return (
     <div className='container'>
       <h4 className="center">Contact</h4>
