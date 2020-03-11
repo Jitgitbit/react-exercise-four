f3d44989ce25e12e8cb45172bbea8965b1fd45f2
Jitgitbit <rowowolf@gmail.com>
2020-03-11 20:17:39 +0100

    3 components added for 3 pages

---
+++ src/components/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Contact() {
+  return (
+    <div className='container'>
+      <h4 className="center">Contact</h4>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+         Dolor cum neque nulla modi itaque minus. 
+         Mollitia voluptate tempora veritatis dignissimos omnis pariatur, nesciunt, recusandae iure quam eius, libero voluptatum dolores.</p>
+    </div>
+  )
+}
