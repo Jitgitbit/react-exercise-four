b80e2067774b5da0936de898fd4711e5a247c89a
Jitgitbit <rowowolf@gmail.com>
2020-03-12 11:28:18 +0100

    post rendered

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -19,9 +19,17 @@ export default class Post extends Component {
     })
   }
   render() {
+    const post = this.state.post ? (
+      <div className="post">
+        <h4 className="center">{this.state.post.title}</h4>
+        <p>{this.state.post.body}</p>
+      </div>
+    ) : (
+      <div className="center">Loading post...</div>
+    )
     return (
       <div className='container'>
-        <h4>info</h4>
+        {post}
       </div>
     )
   }
