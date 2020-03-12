d7152d6a9ed68f88ffc570b1b19cc7a5f863e7ed
Jitgitbit <rowowolf@gmail.com>
2020-03-12 11:03:16 +0100

    that's the route param

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,14 +1,20 @@
 import React, { Component } from 'react'
 
 export default class Post extends Component {
+  state ={
+    id: null
+  }
   componentDidMount(){
     console.log(`=========>> PROPS IN POST:`,this.props)
-    let id = this.props.match.params.post_id
+    let specificId = this.props.match.params.post_id
+    this.setState({
+      id: specificId
+    })
   }
   render() {
     return (
       <div className='container'>
-        <h4>route param</h4>
+        <h4>route param: {this.state.id}</h4>
       </div>
     )
   }
